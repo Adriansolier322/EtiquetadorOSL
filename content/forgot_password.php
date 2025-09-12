70924589f20e4f82eae2888f7bec502431884530
httpsrim <ines.jimenez2003@gmail.com>
2025-09-12 13:28:45 +0200

    V3.1 Implemented 2FA via e-mail

--- content/forgot_password.php
+++ content/forgot_password.php
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $mail->isSMTP();
                 $mail->Host = 'smtp.gmail.com'; // Servidor SMTP; NECESITA SER UNO REAL
                 $mail->SMTPAuth = true;
-                $mail->Username = ---'; // CORREO
+                $mail->Username = '---'; // CORREO
                 $mail->Password = '---'; // CONTRASEÑA, por motivos de seguridad NO se mostrará
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port = 587;
